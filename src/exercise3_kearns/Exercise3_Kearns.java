fa8a485c83a117a10eae54d7b76263e1771b0388
bmk12@students.ptcollege.edu <>
2018-10-22 15:17:32 -0400

    Nya☆Nya

---
+++ src/exercise3_kearns/Exercise3_Kearns.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise3_kearns;
+
+/**
+ *
+ * @author Brandon Kearns
+ */
+public class Exercise3_Kearns {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Kearns();
+    }
+    
+    public static void Kearns() {
+        System.out.println("Brandon☆Kearns");
+    }
+    
+}
